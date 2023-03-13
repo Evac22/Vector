5dbb96759c89cee3069ebd8e921f3eb817e5d1a5
Vlad <94717630+Evac22@users.noreply.github.com>
2023-03-13 16:15:43 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<int> vec; // Создание пустого вектора
+
+    // Инициализация вектора.
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+
+    // Добавление элемента в конец вектора.
+
+    vec.push_back(40);
+
+    //Удаление элемента с конца вектора.
+    vec.pop_back();
+
+    //Поиск элемента в векторе
+    int x = 20;
+    auto it = find(vec.begin(), vec.end(), x);
+    if (it != vec.end()) {
+        cout << "Element " << x << "  Found in vector" << endl;
+    }
+    else {
+        cout << "Element " << x << " Not found in vector" << endl;
+    }
+
+    //Вывод элементов вектора
+    cout << "Vector: ";
+    for (auto elem : vec) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
